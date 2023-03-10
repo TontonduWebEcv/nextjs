63f0ba678f0830cbf8eb8caa2cd37490c62a26dc
Kanelle Lorent <kanelle.lorent@mail-ecv.fr>
2023-03-10 09:39:55 +0100

    update file order.ts

--- interfaces/order.ts
+++ interfaces/order.ts
@@ -1,3 +1,27 @@
-export default interface OrderCreatedDto {
+export interface OrderToCreateDto {
+  products: PurchasedProductDto[];
+}
+
+export interface OrderCreatedDto {
   id: string;
+}
+
+interface PurchasedProductDto {
+  productId: string;
+  quantity: number;
+}
+
+export interface OrderDetailsDto {
+  id: string;
+  status: OrderStatusDto;
+  products: PurchasedProductDto[];
+}
+
+enum OrderStatusDto {
+  Pending = 'Pending',
+  Delivered = 'Delivered',
+}
+
+export interface UpdateOrderDto {
+  status: OrderStatusDto;
 }
