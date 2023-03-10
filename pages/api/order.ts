0a57bc61d8232d88c82baca3f076b05eeadf6c52
Matthieu Dejean <matthieudejean2@gmail.com>
2023-03-10 11:01:13 +0100

    fix: deploy

--- pages/api/order.ts
+++ pages/api/order.ts
@@ -2,11 +2,15 @@ import { PurchasedProductDto } from "../../interfaces/order";
 import { OrderCreatedDto } from "../../interfaces/order";
 import { OrderToCreateDto } from "../../interfaces/order";
 
-const products: PurchasedProductDto[] = [];
+const products: PurchasedProductDto[] = [
+  { productId: "1", quantity: 1 },
+  { productId: "2", quantity: 1 },
+  { productId: "3", quantity: 1 },
+];
 
-// const order: OrderCreatedDto = {
-//   id: "1",
-// };
+const order: OrderCreatedDto = {
+  id: "1",
+};
 
 const orderToCreate: OrderToCreateDto = {
   products,
